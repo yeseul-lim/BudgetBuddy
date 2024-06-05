ced7f0a36f1f6e86b3fd20c06827d50fed44d39c
Aiden Jeehoon On <jeehoono@uci.edu>
2024-06-05 01:15:15 -0700

    Header

--- budgetbuddy/src/Components/organisms/Header/header.jsx
+++ budgetbuddy/src/Components/organisms/Header/header.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+// src/components/Header.js
+import React from 'react';
 import './header.css';
 
 const Header = () => {
-    return (
-        <div>
-            <h1>Header</h1>
-        </div>
-    );
+  return (
+    <header>
+    <img src="logo.png" alt="" />
+
+      <nav className="nav-links">
+        <a href="#home">Home</a>
+        <a href="#expense">Expense</a>
+        <a href="#budget">Budget</a>
+        <a href="#setting">Setting</a>
+        <a href="#login">Log in</a>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;

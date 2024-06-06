3aedcf796bf5f3450585b60eab821632a1bc733a
Yeseul Lim <97212209+yeseul-lim@users.noreply.github.com>
2024-06-06 16:40:02 -0700

    done with add account

--- budgetbuddy/src/Pages/AddAccount/addAccount.jsx
+++ budgetbuddy/src/Pages/AddAccount/addAccount.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import './addAccount.css';
 
 const AddAccount = () => {
+    const [droppedImage, setDroppedImage] = useState(null);
+
+    const handleDragStart = (e, id) => {
+        e.dataTransfer.setData("text/plain", id);
+    };
+
+    const handleDrop = (e) => {
+        const id = e.dataTransfer.getData("text");
+        setDroppedImage(id);
+        e.preventDefault();
+    };
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    };
+
+    const getImagePath = (id) => {
+        const imageMap = {
+            "1": "/Chase.png",
+            "2": "/BOA.png",
+            "3": "/wellsfargo.png",
+            "4": "/amex.png",
+            "5": "/citi.png",
+            "6": "/usbank.png"
+        };
+        return imageMap[id];
+    };
+
     return (
         <div className="addAccount-container">
             <div className="addAccount-box">
@@ -9,12 +37,19 @@ const AddAccount = () => {
                 <section className="add-account-section">
                     <div className="find-bank">
                         <div className="bank-images">
-                            <img src="/Chase.png" alt="Bank 1" />
-                            <img src="/BOA.png" alt="Bank 2" />
-                            <img src="/wellsfargo.png" alt="Bank 3" />
-                            <img src="/amex.png" alt="Bank 4" />
-                            <img src="/citi.png" alt="Bank 5" />
-                            <img src="/usbank.png" alt="Bank 6" />
+                            <img src="/Chase.png" alt="Bank 1" draggable onDragStart={(e) => handleDragStart(e, "1")} />
+                            <img src="/BOA.png" alt="Bank 2" draggable onDragStart={(e) => handleDragStart(e, "2")} />
+                            <img src="/wellsfargo.png" alt="Bank 3" draggable onDragStart={(e) => handleDragStart(e, "3")} />
+                            <img src="/amex.png" alt="Bank 4" draggable onDragStart={(e) => handleDragStart(e, "4")} />
+                            <img src="/citi.png" alt="Bank 5" draggable onDragStart={(e) => handleDragStart(e, "5")} />
+                            <img src="/usbank.png" alt="Bank 6" draggable onDragStart={(e) => handleDragStart(e, "6")} />
+                        </div>
+                        <div
+                            className="drop-box"
+                            onDrop={handleDrop}
+                            onDragOver={handleDragOver}
+                        >
+                            {droppedImage ? <img src={getImagePath(droppedImage)} alt="Dropped" /> : "Drop here"}
                         </div>
                     </div>
                     <div className="sign-in">
